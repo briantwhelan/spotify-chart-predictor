c1b00e7c5636ffd1480728e011f1f66c2062ee84
briantwhelan <whelanb8@tcd.ie>
2022-11-22 12:06:10 +0000

    Extract a single track's audio features from Spotify API
    This commit adds a simple program to retrieve the track id for a
    specified track from the Spotofy API and then uses this id to extract
    the audio features for that track from the API. Environment variables
    must be configured in an accompanying .env file.

---
+++ extract_data.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+BASE_URL = 'https://api.spotify.com/v1/'
+
+# Load environment variables.
+load_dotenv()
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+# Authenticate.
+AUTH_URL = "https://accounts.spotify.com/api/token"
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+auth_response_data = auth_response.json()
+access_token = auth_response_data['access_token']
+
+# Need to pass access token into header to send properly formed GET request to API server
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+# Get the track id for the specified track.
+def get_track_id(artist, track):
+  response = requests.get(BASE_URL + 'search?q=artist:' + artist + '%20track:' + track + '&type=track', headers=headers)
+  response_json = response.json()
+  return response_json['tracks']['items'][0]['id']
+
+# Get the audio features for the specified track.
+def get_track_audio_features(track):
+  response = requests.get(BASE_URL + 'audio-features/' + track, headers=headers)
+  response_json = response.json()
+  return response_json
+
+track = get_track_id("Taylor Swift", "Fearless (Taylor's Version)")
+features = get_track_audio_features(track)
+print(features)
