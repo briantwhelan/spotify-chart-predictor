772ce06465197f88e35a42eefae05a3ca07c328c
briantwhelan <whelanb8@tcd.ie>
2022-11-29 10:41:55 +0000

    Add logistic regression
    This commit adds a logistic regression classifier with cross validation
    to select the two hyperparameters, q and C. The plots from the cross
    validation are stored in a new plots directory. The performance of the
    logistic regression appears to be no better than the baseline currently
    but there is not enough data to assess this properly.

--- code/chart_predictor.py
+++ code/chart_predictor.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
@@ -20,3 +26,48 @@ baseline_model.fit(X_train, y_train)
 baseline_preds = baseline_model.predict(X_validate)
 print(confusion_matrix(y_validate, baseline_preds))
 print(classification_report(y_validate, baseline_preds))
+
+# Perform cross validation to select q.
+mean_error = []
+std_error = []
+q_range = [1, 2, 3, 4, 5, 6]
+kf = KFold(n_splits=5)
+for q in q_range:
+  X_poly = PolynomialFeatures(q).fit_transform(X_train)
+  model = LogisticRegression(penalty="l2", C=1)
+  scores = cross_val_score(model, X_poly, y_train, cv=kf, scoring="f1")
+  mean_error.append(np.array(scores).mean())
+  std_error.append(np.array(scores).std())
+plt.rc("font", size=18)
+plt.errorbar(q_range, mean_error, yerr=std_error, linewidth=3)
+plt.xlabel("q")
+plt.ylabel("F1 Score")
+plt.title("q Cross Validation for Logistic Regression Model")
+plt.legend()
+plt.savefig("./plots/lr-q-cross-validation.png")
+
+# Perform cross validation to select C.
+mean_error = []
+std_error = []
+c_range = [0.01, 0.1, 1, 10, 100]
+kf = KFold(n_splits=5)
+for c in c_range:
+  model = LogisticRegression(penalty="l2", C=c)
+  scores = cross_val_score(model, X_train, y_train, cv=kf, scoring="f1")
+  mean_error.append(np.array(scores).mean())
+  std_error.append(np.array(scores).std())
+plt.rc("font", size=18)
+plt.rcParams["figure.constrained_layout.use"] = True
+plt.errorbar(c_range, mean_error, yerr=std_error, linewidth=3)
+plt.xlabel("C")
+plt.ylabel("F1 Score")
+plt.title("C Cross Validation for Logistic Regression Model")
+plt.legend()
+plt.savefig("./plots/lr-C-cross-validation.png")
+
+# Train logistic regression classifier (with L2 penalty) with hyperparameters from cross-validation.
+lrl2_model = LogisticRegression(penalty="l2", C=1000)
+lrl2_model.fit(X_train, y_train)
+lrl2_preds = lrl2_model.predict(X_validate)
+print(confusion_matrix(y_validate, lrl2_preds))
+print(classification_report(y_validate, lrl2_preds))
