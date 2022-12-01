b4e08ef8c2ddea0574f7901aa24cd5a212e60485
James Deasley <deasleyj@tcd.ie>
2022-12-01 03:46:15 +0000

    Remove bracketed text from track titles

--- code/charts_utilities.py
+++ code/charts_utilities.py
@@ -1,7 +1,7 @@
 # Find next closed bracket after given index
-def _next_close_bracket(string: str, after: int) -> int:
+def _find_next(string: str, sub: str, after: int) -> int:
     cut_string = string[after:]
-    return cut_string.find(')') + after
+    return cut_string.find(sub) + after
 
 # Typical patterns in track names for featured artists
 FEATURED_PATTERNS = [
@@ -14,20 +14,37 @@ def _remove_featured_artists(track_name: str) -> str:
     lower = track_name.lower()
     for pattern in FEATURED_PATTERNS:
         # Check for "feat " or "feat.", not just "feat" in case of words like "feather"
-        cut_index = max([lower.find('(' + pattern + ' '), lower.find('(' + pattern + '.')])
+        # cut_index = max([lower.find('(' + pattern + ' '), lower.find('(' + pattern + '.')])
 
-        if(cut_index != -1):
-            # print(f'- {pattern} tag at {cut_index} -> "{track_name[cut_index]}{track_name[cut_index+1]}"')
-            next_brack = _next_close_bracket(lower, cut_index)
-            # print(f'- finished at {next_brack} -> "{track_name[next_brack - 1]}{track_name[next_brack]}"')
-            return track_name[:cut_index].strip() + track_name[next_brack + 1:]
+        # if(cut_index != -1):
+        #     # print(f'- {pattern} tag at {cut_index} -> "{track_name[cut_index]}{track_name[cut_index+1]}"')
+        #     next_brack = _find_next(lower, '(', cut_index)
+        #     # print(f'- finished at {next_brack} -> "{track_name[next_brack - 1]}{track_name[next_brack]}"')
+        #     return track_name[:cut_index].strip() + track_name[next_brack + 1:]
         
         # max because it will be one or the other, so one will be -1
-        cut_index = max([track_name.find(' ' + pattern + ' '), track_name.find(' ' + pattern + '.')])
+        cut_index = max([lower.find(' ' + pattern + ' '), lower.find(' ' + pattern + '.')])
         if (cut_index != -1):
             return track_name[:cut_index].strip()
 
     return track_name
+
+OPEN_BRACKETS = ['(','[','{']
+CLOSE_BRACKETS = [')','}','}']
+# Remove anything in brackets
+# For songs with alternate names within brackets
+# Or (feat. Artist) tags
+def _remove_brackets(track_name: str) -> str:
+    cleaned_str = track_name
+    
+    for i, brack in enumerate(OPEN_BRACKETS):
+        cut_start = cleaned_str.find(brack)
+
+        if cut_start != -1:
+            cut_end = _find_next(cleaned_str, CLOSE_BRACKETS[i], cut_start)
+            cleaned_str = (cleaned_str[:cut_start] + cleaned_str[cut_end + 1:]).strip()
+
+    return cleaned_str.strip()
 
 # Remove tags that indicate a track is from a movie or show
 # e.g. Song Name - From Frozen
@@ -53,7 +70,7 @@ def clean_title(track_name: str) -> str:
     """
     # Don't pass as lower because wan't to keep original case for scraping
     # Only use lower within individual functions for pattern matching
-    return _remove_from_tag(_remove_featured_artists(track_name))
+    return _remove_from_tag(_remove_featured_artists(_remove_brackets(track_name)))
 
 def _remove_the(artist_name: str) -> str:
     lower = artist_name.lower()
