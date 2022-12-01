56d6dc2c3149759cb79e10ea1f65b643fff77444
James Deasley <deasleyj@tcd.ie>
2022-12-01 02:22:21 +0000

    Set up code and CSVs for parallel classifications

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@ from spotify_data import extract_features, FEATURES_TO_EXTRACT
 import pandas as pd
 import csv
 import os
+from sys import argv
 
 # Create duplicate free copy of csv file
 def remove_duplicates(filename_in: str, filename_out: str) -> None:
@@ -11,21 +12,19 @@ def remove_duplicates(filename_in: str, filename_out: str) -> None:
     no_dupes.to_csv(filename_out, index=False)
 
 # Classify tracks in specified file as charted or not
-def classify_training_tracks(filename) -> None:
+def classify_training_tracks(in_filename: str, out_charted_filename: str, out_uncharted_filename: str) -> None:
     # Import list of songs.
-    data = pd.read_csv(filename)
+    data = pd.read_csv(in_filename)
     songs = data.to_numpy()
 
-    filename_charted = "./data/charted_2.csv"
-    filename_uncharted = "./data/uncharted_2.csv"
-
-    if(os.path.exists(filename_charted) and os.path.isfile(filename_charted)):
-        os.remove(filename_charted)
-    if(os.path.exists(filename_uncharted) and os.path.isfile(filename_uncharted)):
-        os.remove(filename_uncharted)
+    if(os.path.exists(out_charted_filename) and os.path.isfile(out_charted_filename)):
+        os.remove(out_charted_filename)
+    if(os.path.exists(out_uncharted_filename) and os.path.isfile(out_uncharted_filename)):
+        os.remove(out_uncharted_filename)
 
     # Fields for writing to csv file.
-    fields = ["track","artist","charted",*(FEATURES_TO_EXTRACT)] 
+    # fields = ["track","artist","charted",*(FEATURES_TO_EXTRACT)] 
+    fields = ["track","artist","charted"] 
 
     # Extract features from Spotify and classification from officialcharts.
     charted_songs = []
@@ -39,44 +38,54 @@ def classify_training_tracks(filename) -> None:
         # Check for empty entries
         # (NaN is only value that evaluates as not equal to itself)
         if (track != track) or (artist != artist):
-            print(f'NO DATA')
+            print(f'MISSING DATA')
             continue
 
         found, charted = has_charted(track, artist)
         if found and charted:
             print(f"YES - Has charted in Top {CHARTING_THRESHOLD}")
-            track_features = extract_features(track_id)
+            # track_features = extract_features(track_id)
             # For each feature field: Value OR None
-            charted_songs.append([track,artist,True,*(track_features.get(x,None) for x in FEATURES_TO_EXTRACT)])
+            # charted_songs.append([track,artist,True,*(track_features.get(x,None) for x in FEATURES_TO_EXTRACT)])
+            charted_songs.append([track_id,True])
         elif found:
             print(f"NO - Has not charted in Top {CHARTING_THRESHOLD}")
-            track_features = extract_features(track_id)
+            # track_features = extract_features(track_id)
             # For each feature field: Value OR None
-            uncharted_songs.append([track,artist,False,*(track_features.get(x,None) for x in FEATURES_TO_EXTRACT)])
+            # uncharted_songs.append([track,artist,False,*(track_features.get(x,None) for x in FEATURES_TO_EXTRACT)])
+            uncharted_songs.append([track_id,False])
         else:
             print('NOT FOUND')
 
         # Write to CSV files every 100 tracks for safety
         if i % 100 == 0:
-            with open(filename_charted, 'w') as csvfile:  
+            with open(out_charted_filename, 'w') as csvfile:  
                 csvwriter = csv.writer(csvfile, lineterminator='\n') 
                 csvwriter.writerow(fields) 
                 csvwriter.writerows(charted_songs)
 
-            with open(filename_uncharted, 'w') as csvfile:  
+            with open(out_uncharted_filename, 'w') as csvfile:  
                 csvwriter = csv.writer(csvfile, lineterminator='\n') 
                 csvwriter.writerow(fields) 
                 csvwriter.writerows(uncharted_songs)
     
-    with open(filename_charted, 'w') as csvfile:  
+    with open(out_charted_filename, 'w') as csvfile:  
         csvwriter = csv.writer(csvfile, lineterminator='\n') 
         csvwriter.writerow(fields) 
         csvwriter.writerows(charted_songs)
 
-    with open(filename_uncharted, 'w') as csvfile:  
+    with open(out_uncharted_filename, 'w') as csvfile:  
         csvwriter = csv.writer(csvfile, lineterminator='\n') 
         csvwriter.writerow(fields) 
         csvwriter.writerows(uncharted_songs)
     
 # remove_duplicates("./data/recommendations.csv", "./data/recs_no_duplicates.csv")
-classify_training_tracks("./data/recs_no_duplicates.csv")
+
+if __name__ == '__main__':
+    file_to_classify = argv[1]
+    print(f'Classifying tracks in file {file_to_classify}...')
+    classify_training_tracks(
+        in_filename=f"./data/recs/recs_{file_to_classify}.csv",
+        out_charted_filename=f"./data/charted/charted_{file_to_classify}.csv",
+        out_uncharted_filename=f"./data/uncharted/uncharted_{file_to_classify}.csv"
+    )
